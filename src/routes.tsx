db8c769288246880ea30f0aafd983d3ca07d0aa6
sweedfengpat <b6105358@g.sut.ac.th.com>
2023-06-20 08:20:17 +0700

    phase 1

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,10 @@ import NFTMarketplace from "views/admin/marketplace";
 import Profile from "views/admin/profile";
 import DataTables from "views/admin/tables";
 import RTLDefault from "views/rtl/default";
+import Finace from "views/admin/finace";
+import Sell from "views/admin/sell";
+import Selllist from "views/admin/selllist";
+import Stockphama from "views/admin/stockphama";
 
 // Auth Imports
 import SignIn from "views/auth/SignIn";
@@ -41,6 +45,38 @@ const routes = [
   //   icon: <MdBarChart className="h-6 w-6" />,
   //   path: "data-tables",
   //   component: <DataTables />,
+  // },
+  {
+    name: "การเงิน",
+    layout: "/admin",
+    icon: <MdBarChart className="h-6 w-6" />,
+    path: "finace",
+    component: <Finace />,
+  },{
+    name: "การขาย",
+    layout: "/admin",
+    icon: <MdBarChart className="h-6 w-6" />,
+    path: "sell",
+    component: <Sell />,
+  },{
+    name: "รายการสั่งซื้อ",
+    layout: "/admin",
+    icon:  <MdOutlineShoppingCart className="h-6 w-6" />,
+    path: "selllist",
+    component: <Selllist />,
+  },{
+    name: "คลังยา",
+    layout: "/admin",
+    icon:  <MdOutlineShoppingCart className="h-6 w-6" />,
+    path: "stockphama",
+    component: <Stockphama />,
+  },
+  // {
+  //   name: "NFT Marketplace",
+  //   layout: "/admin",
+  //   path: "nft-marketplace",
+  //   icon: <MdOutlineShoppingCart className="h-6 w-6" />,
+  //   component: <NFTMarketplace />,
   // },
   // {
   //   name: "Profile",
